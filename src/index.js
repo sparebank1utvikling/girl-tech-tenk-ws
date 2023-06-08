b9e6f3635ed503c23da7e39236dff76c70084711
kariannekristiansen <karianne.kristiansen@sparebank1.no>
2023-06-08 14:00:40 +0200

    Oppdatere navigasjon og forside

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./main.css";
-import {AccountDetails} from "./components/ExampleOverview/AccountDetails";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 import App from "./App";
 import Oversikt from "./features/oversikt/Oversikt";
 import Sparing from "./features/sparing/Sparing";
 import Navigation from "./components/navigation";
-import Betaling from "./features/betaling/Betaling";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -16,10 +14,11 @@ root.render(
         <Navigation/>
         <BrowserRouter>
             <Routes>
+                {/*todo fiks */}
                 <Route path="/" element={<App/>}/>
-                <Route path="/sparing" element={<Sparing/>}/>
-                <Route path="/oversikt" element={<Oversikt/>}/>
-                <Route path="/betaling" element={<Betaling/>}/>
+                <Route path="/banken" element={<Sparing/>}/>
+                <Route path="/slik-skal-det-se-ut" element={<Oversikt/>}/>
+                {/*<Route path="/betaling" element={<Betaling/>}/>
                 <Route path="/betaling/:accountNumber" element={<AccountDetails/>}/>
 
                 {/*Overfør blir ikke brukt*/}
