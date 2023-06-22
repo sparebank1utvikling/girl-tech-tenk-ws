12919e9e155ef7f96b300972421d9146fec87f39
kariannekristiansen <karianne.kristiansen@sparebank1.no>
2023-06-22 11:26:15 +0200

    Implement tabGroup

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./App.less";
-import {AccountDetails} from "./components/ExampleOverview/AccountDetails.jsx";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 import App from "./App";
-import Oversikt from "./features/oversikt/Oversikt.jsx";
 import Navigation from "./components/navigation.jsx";
-import Betaling from "./features/betaling/Betaling.jsx";
 import SlikSkalDetSeUt from "./features/slik-skal-det-se-ut/SlikSkalDetSeUt.jsx";
+import Banken from "./features/banken/Banken.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -17,10 +15,8 @@ root.render(
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<App/>}/>
-                <Route path="/banken" element={<Oversikt/>}/>
+                <Route path="/banken" element={<Banken/>}/>
                 <Route path="/slik-skal-det-se-ut" element={<SlikSkalDetSeUt/>}/>
-                <Route path="/betaling" element={<Betaling/>}/>
-                <Route path="/betaling/:accountNumber" element={<AccountDetails/>}/>
             </Routes>
         </BrowserRouter>
     </div>
